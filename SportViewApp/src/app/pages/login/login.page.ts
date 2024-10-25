ee78d4dfb02bc7a4345d4563dea412c1f7fce45f
Javier Molina <javier.molina@mail.udp.cl>
2024-10-25 02:18:52 -0300

    Working on login logic

--- SportViewApp/src/app/pages/login/login.page.ts
+++ SportViewApp/src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginCredentials } from 'src/app/models/users.models';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +12,33 @@ export class LoginPage implements OnInit {
 
   sportViewLogo: string = '../../../assets/sportview-logo.png';
 
-  constructor(private router: Router) { }
+  badCredentials: boolean = false;
+
+  loginCredentials: LoginCredentials = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private router: Router, private userService: UserService) { }
 
   ngOnInit() {
   }
 
   login() {
     // implement login validation
-    
+    this.userService.login(this.loginCredentials).subscribe(
+      (data) => {
+        console.log('Login successful:', data);
+        this.badCredentials = false;
+      },
+      (error) => {
+        if (error.status == 401) {
+          console.log('Invalid login credentials');
+          this.badCredentials = true;
+        }
+        else console.log('An unexpected error occurred. Please try again.');
+      }
+    )
   }
 
   goToResetPassword() {
