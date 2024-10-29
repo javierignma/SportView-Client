a04c686bd595cd466706cd5456fa4b66adfa1ec1
Javier Molina <javier.molina@mail.udp.cl>
2024-10-29 02:06:27 -0300

    add token handler

--- SportViewApp/src/app/services/user.service.ts
+++ SportViewApp/src/app/services/user.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { api, routes } from 'src/environments/environment';
 import { LoginCredentials, User } from '../models/users.models';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,39 @@ import { Observable } from 'rxjs';
 export class UserService {
   apiUsersRoute: string = api.backend+routes.users;
 
-  constructor(private http: HttpClient) { }
+  currentUser?: User;
+
+  headers: HttpHeaders;
+
+  constructor(private http: HttpClient) { 
+    this.headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + localStorage.getItem('auth_token')
+    });
+  }
+
+  setToken(token: string) {
+    console.log("[user service - setToken] token: "+token);
+    localStorage.setItem('auth_token', token);
+    this.headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + token
+    });
+  }
 
   createNewUser(user: User): Observable<User> {
     return this.http.post<User>(this.apiUsersRoute, user);
   }
 
   login(loginCredentials: LoginCredentials): Observable<User> {
-    return this.http.post<User>(this.apiUsersRoute+'login/', loginCredentials);
+    return this.http.post<User>(this.apiUsersRoute+'login/', loginCredentials).pipe(
+      tap((response: any) => {
+        this.setToken(response.access_token);
+        this.currentUser = response;
+      })
+    );
+  }
+
+  verifyToken() {
+    const headers = this.headers;
+    return this.http.get(this.apiUsersRoute+'verify-token/', { headers })
   }
 }
