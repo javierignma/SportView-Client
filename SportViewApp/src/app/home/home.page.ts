6223a047081b2df3fdac28b3f161e4d206b33e1d
Javier Molina <javier.molina@mail.udp.cl>
2024-11-03 23:15:19 -0300

    Working on home page

--- SportViewApp/src/app/home/home.page.ts
+++ SportViewApp/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TabsComponent } from '../components/tabs/tabs.component';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,12 @@ export class HomePage {
 
   sportViewLogo: string = '../../../assets/sportview-logo.png';
 
-  constructor() {}
+  name: string = '';
+
+  constructor(private userService: UserService) {}
+
+  ionViewWillEnter() {
+    this.name = this.userService.getCurrentUser('first_name');
+  }
 
 }
